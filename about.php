2f72a204b5f5a20d3582841d0642a011de682fbe
xWoozixx <127356493+xWoozixx@users.noreply.github.com>
2023-09-10 13:15:29 +1100

    Create about.php

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Site Wakfu Thème - À propos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    
+    <div class="content">
+        <h1>À propos de nous</h1>
+        <p>Nous sommes passionnés par l'univers de Wakfu et souhaitons partager notre amour pour ce thème coloré.</p>
+    </div>
+</body>
+</html>
