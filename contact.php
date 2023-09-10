fd6b29b6d0aa902bd41b0776c6b1b3dd7abe3c4a
xWoozixx <127356493+xWoozixx@users.noreply.github.com>
2023-09-10 13:17:01 +1100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Site Wakfu Thème - Contact</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    
+    <div class="content">
+        <h1>Contactez-nous</h1>
+        <p>N'hésitez pas à nous contacter si vous avez des questions ou des commentaires sur notre site Wakfu Thème.</p>
+    </div>
+</body>
+</html>
