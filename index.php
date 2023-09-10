cc0b15ec944a165eeb83364a19f059e2ed763424
xWoozixx <127356493+xWoozixx@users.noreply.github.com>
2023-09-10 13:14:45 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Site Wakfu Thème - Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    
+    <div class="content">
+        <h1>Bienvenue sur notre site Wakfu Thème</h1>
+        <p>Ceci est la page d'accueil. Profitez du contenu lié à l'univers coloré de Wakfu.</p>
+    </div>
+</body>
+</html>
