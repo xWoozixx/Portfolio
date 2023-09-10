c58da64b32b6008ed02963fb282a997c62e5d7d2
xWoozixx <127356493+xWoozixx@users.noreply.github.com>
2023-09-10 13:16:25 +1100

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Site Wakfu Thème - Galerie</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    
+    <div class="content">
+        <h1>Galerie d'images Wakfu</h1>
+        <div class="image-gallery">
+            <img src="images/image1.jpg" alt="Image 1">
+            <img src="images/image2.jpg" alt="Image 2">
+            <!-- Ajoutez davantage d'images ici -->
+        </div>
+    </div>
+</body>
+</html>
