dd33b21ee23685db1c47e8a85f3407c8196f7911
xWoozixx <127356493+xWoozixx@users.noreply.github.com>
2023-09-10 13:17:54 +1100

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <h1>Site Wakfu Thème</h1>
+</div>
+
+<div class="menu">
+    <a href="index.php">Accueil</a>
+    <a href="about.php">À propos</a>
+    <a href="gallery.php">Galerie</a>
+    <a href="contact.php">Contact</a>
+</div>
