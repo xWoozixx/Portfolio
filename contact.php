70024f33b051e713da00263fe48a362028f71911
xWoozixx <r.jauneau13@gmail.com>
2023-09-13 10:17:31 +1100

    0.2

--- contact.php
+++ contact.php
@@ -2,8 +2,14 @@
 <?php include 'header.php'; ?>
 <body>
     <div class="content">
-        <h1>Contactez-moi</h1>
-        <p>N'hésitez pas à me contacter</p>
+        <h1>Vous pouvez me contacter</h1><br>
+        <ul>
+            <p>Par Mail :</p>
+            <li><a href=mailto:r.jauneau13@gmail.com>r.jauneau13@gmail.com</a></li><br>
+
+            <p>Me suivre sur :</p> 
+            <li><a href="https://www.linkedin.com/in/jauneau-r%C3%A9mi-425028247">LinkedIn</a></li>
+        </ul>
     </div>
 </body>
 </html>
