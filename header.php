209fd90c87b151ed71d3f79fce1a18ceef12c96c
xWoozixx <r.jauneau13@gmail.com>
2023-09-13 09:51:54 +1100

    0.1

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
     <nav class="menu">
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="about.php">À propos</a></li>
-            <li><a href="gallery.php">Galerie</a></li>
+            <li><a href="parcours.php">Mon parcours</a></li>
+            <li><a href="projet.php">Projets</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
