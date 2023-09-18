52c851db9906f542852214b5e46f74f57dd12bf7
xWoozixx <r.jauneau13@gmail.com>
2023-09-18 15:48:54 +1100

    0.6

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     <div class="content">
         <h1 id="Titre-1">Ce site est en cours de développement</h1>
         <p>Texte</p>
+
+
+
+
+        <br><br>
+        <a href="JAUNEAU_Rémi_Tableau_de_synthèse_Epreuve_E4_BTS_SIO_2023.pdf">Tableau de synthèse</a>
     </div>
 </body>
 
--- projet.php
+++ projet.php
@@ -3,12 +3,65 @@
 
 <body>
     <div class="content">
-        <h1 id="Titre-1">Voici un récapitulatif des étapes principales</h1><br><br>
+        <h1 id="Titre-1">Etude de mails de phishing</h1><br><br>
 
         <div class="images">
             <!-- Cette div permet de mettre le texte au dessus des images -->
 
-            <h1>2015</h1>
+            <h1>Exemple de mail reçu  </h1>
+            <div class="image-parcours">
+                <img src="images/phishing.png" alt="phishing">
+            </div>
+
+            <br><br><h1>Exemple de pdf dans un mail  </h1>
+
+            <div class="image-parcours">
+                <img src="images/pdfphishing.png" alt="pdfphishing">
+            </div>
+
+            <br><br><h1>Synthèse des informations trouvées par AppAnyRun  </h1>
+
+            <div class="image-parcours">
+                <img src="images/appanyrun.png" alt="appanyrun">
+            </div>
+
+            <br><br><h1>Synthèse des informations du pdf (trouvés grace à l'outil poppler utils)</h1>
+
+            <div class="image-parcours">
+                <img src="images/pdfinfo.png" alt="pdfinfo">
+            </div>
+        </div>
+        
+        <br><br>
+        <h1 id="Titre-1">Etude de marché</h1><br><br>
+        <div class="images">
+            <!-- Cette div permet de mettre le texte au dessus des images -->
+
+            <h1>Etude de Benchmark reconnus</h1>
+
+            <div class="image-parcours">
+                <img src="images/forrester.png" alt="kanban">
+            </div>
+
+            <div class="image-parcours">
+                <img src="images/gartner.png" alt="kanban">
+            </div>
+
+            <br><br><h1>Tableau de synthèse</h1>
+
+            <div class="image-parcours">
+                <img src="images/tableauRapportEtudes.png" alt="kanban">
+            </div>
+
+        </div>
+
+        <br><br>
+        <h1 id="Titre-1">Kubernetes</h1><br><br>
+
+        <div class="images">
+            <!-- Cette div permet de mettre le texte au dessus des images -->
+
+            <h1>Texte</h1>
             <div class="image-parcours">
                 <img src="images/kanban.png" alt="kanban">
             </div>
