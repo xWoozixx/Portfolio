09429220ddd447be874e9a51fb71e388d78649bb
xWoozixx <r.jauneau13@gmail.com>
2023-09-27 20:45:11 +1100

    1.0

--- veille.php
+++ veille.php
@@ -3,19 +3,33 @@
 
 <body>
     <div class="content">
-        <h1 id="Titre-1">Mon parcours</h1><br><br>
+        <h1 id="Titre-1">Voici des exemples d'articles qui ont attirés mon attention</h1><br><br>
 
         <div class="images">
             <!-- Cette div permet de mettre le texte au dessus des images -->
 
-            <h1>Doc 1</h1>
+            <h1>Le stockage du futur ?</h1>
             <div id="veille1" class="image-parcours">
-                <img src="images/lien de l'image.jpg" alt="veille1">
+            <a href="https://youtu.be/KMg2tQ511d4?si=Tojzo033-_pw0dQC&t=390" target="_blank">
+                <img src="images/veille/stockageFutur.png" alt="veille1">
+            </a><br>
             </div>
             <br><br>
-            <a href="https://youtu.be/KMg2tQ511d4?si=Tojzo033-_pw0dQC&t=390">Le stockage du futur ?</a>
+            
+            <a href="https://bigmedia.bpifrance.fr/decryptages/ladn-le-futur-durable-du-stockage-de-donnees-numeriques" target="_blank">L'article lié au sujet</a>
 
             <br><br>
+            <h1>Des hackeurs ingénieux ?</h1>
+
+            <a href="https://www.macg.co/ailleurs/2023/04/la-signature-thermique-des-touches-dun-clavier-peut-reveler-un-mot-de-passe-136425" target="_blank">
+                Récupérer un mot de passe avec une photo infrarouge
+            </a><br><br>
+            <a href="https://www.numerama.com/cyberguerre/1176886-les-images-thermiques-sont-un-bon-moyen-de-derober-un-mot-de-passe.html" target="_blank">
+                Fonctionne aussi avec les schema de téléphones
+            </a><br><br>
+            <a href="https://www.silicon.fr/la-chaleur-des-doigts-pour-pirater-le-code-pin-des-smartphones-170097.html" target="_blank">
+                Et les codes PIN
+            </a><br><br>
         </div>
     </div>
 </body>
