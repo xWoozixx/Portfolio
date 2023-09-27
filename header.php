f27121f89348f96b3d3e583e41cde0e1b51f2210
xWoozixx <r.jauneau13@gmail.com>
2023-09-27 12:06:15 +1100

    tkt mon reuf

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
         <ul>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="projet.php">Projets</a></li>
+            <li><a href="veille.php">Veille</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
---
+++ veille.php
@@ -0,0 +1,23 @@
+<?php include 'head.php'; ?>
+<?php include 'header.php'; ?>
+
+<body>
+    <div class="content">
+        <h1 id="Titre-1">Mon parcours</h1><br><br>
+
+        <div class="images">
+            <!-- Cette div permet de mettre le texte au dessus des images -->
+
+            <h1>Doc 1</h1>
+            <div id="veille1" class="image-parcours">
+                <img src="images/lien de l'image.jpg" alt="veille1">
+            </div>
+            <br><br>
+            <a href="https://youtu.be/KMg2tQ511d4?si=Tojzo033-_pw0dQC&t=390">Le stockage du futur ?</a>
+
+            <br><br>
+        </div>
+    </div>
+</body>
+
+</html>
